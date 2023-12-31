2e553eb8bab1e3a1b3fc5a6464e6e01147fc8b38
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2023-12-31 15:50:52 -0500

    "Updated js file to show file type and size"

--- jsfileupload.js
+++ jsfileupload.js
@@ -1,13 +1,41 @@
-const inputElement = document.getElementById('fileInput');
-
-inputElement.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    
-    console.log('File name:', file.name);
-    console.log('File size (in bytes):', file.size);
-    console.log('File type:', file.type);
-});
 
 
+ /* var loadFile = function(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+  };
+  */
 
-
+  myfunction=()=>{
+  var x = document.getElementById("file");
+  var txt = "";
+  if('files' in x){
+    if(x.files.length == 0){
+        txt = "Select One More Than One Files";
+        }
+    else{
+        for(var i=0; i<x.files.length; i++){
+            txt += "<br> <strong>" + (i+1) + ".File </strong> <br>";
+            var file = x.files[i];
+            if ('name' in file){
+                txt += "Name: " + file.name + "<br>";    
+            }
+            if ('size' in file){
+                txt += "Size: " + file.size + "bytes <br>";
+            }
+            if ('type' in file){
+                txt += "Type: " + file.type + "<br>";
+            }
+        }
+    }
+}
+    else{
+        if (x.value ==""){
+            txt += "Select One or More Than One Files";
+        }
+        else{
+            txt += "The File Property is not supported in your Browser";
+        }
+    }
+    document.getElementById("demo").innerHTML = txt;
+    }
